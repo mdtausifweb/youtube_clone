a8545800146a97bdc05a7f73f0de2d9ebe0844db
DESKTOP-R8C4C0L\mdtausif <tausif.intern@yum.si>
2023-08-31 22:00:56 +0530

    code change

--- src/Redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-// import thunk from "thunk"
-import { VideoReducers } from "./VideoReducer/Reducers"
- const store = configureStore({
-    reducer: {
-        VideoArr: VideoReducers
-    }
-})
-export default store
